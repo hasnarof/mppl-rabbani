11745f1bbfd0260178a41443e53a41204f23e8a6
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2021-11-27 19:03:45 +0700

    feat: apply repository to Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -14,31 +14,4 @@ class Product extends Model
     {
         return $this->hasMany(ProductDetail::class);
     }
-
-    public function productDetailsByColor()
-    {
-        $product_details = DB::select(DB::raw("
-            SELECT warna, harga, image
-            FROM product_details
-            WHERE product_id = ".$this->id."
-            GROUP BY warna, harga, image
-            order by id
-        "));
-
-        return $product_details;
-    }
-
-    public function productDetailsBySize()
-    {
-        $product_details = DB::select(DB::raw("
-            SELECT ukuran, harga
-            FROM product_details
-            WHERE product_id = ".$this->id."
-            GROUP BY ukuran, harga
-            order by id
-        "));
-
-        return $product_details;
-    }
-
 }
